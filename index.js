386b051364cecc4542a03922b573e73b9b14bb33
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-27 22:22:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function moveDodgerRight()
 {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
-  if(left < 400){}
+  if(left < 360)
+  {
+    dodger.style.left = `${left + 1}px`;
+  }
 }
 
 document.addEventListener('keydown', function(e){
